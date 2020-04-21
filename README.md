74ee907f09aacd7553563375e90be2d0173bfe38
Samantha Ko <samanthako@Samanthas-MacBook-Pro.local>
2020-04-21 15:40:02 -0400

    added files

---
+++ README.md
@@ -0,0 +1,10 @@
+# intro-html-css
+Facilitator Code
+
+SIP U3L1 
+
+All of the exercises in the Intro to HTML/CSS activity
+
+by Girls Who Code
+
+March 2020
---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+intro-html-css
+Facilitator Code
+SIP U3L1 
+
+Sample code for intro to HTML & CSS
+by Girls Who Code
+March 2020
+*/
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
